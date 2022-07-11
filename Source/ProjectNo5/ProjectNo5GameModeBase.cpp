3b9d42170b21c154f04fc9ff7b4aa70a71b764bf
LDWGameDev <100270827+LDWGameDev@users.noreply.github.com>
2022-07-11 16:54:00 +0700

    Added new project

---
+++ Source/ProjectNo5/ProjectNo5GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "ProjectNo5GameModeBase.h"
+
---
+++ Source/ProjectNo5/ProjectNo5GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectNo5GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTNO5_API AProjectNo5GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
